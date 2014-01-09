4ab3162e1d6e5ffed49577867a635c3c0ff264ad
Pierre <pierrerohel@gmail>
2014-01-09 15:39:31 -0500

    implementing login

--- login.php
+++ login.php
@@ -7,8 +7,24 @@
 	<title>Page Title</title>
 </head>
 <body>
+<span id="signinButton">
+  <span
+    class="g-signin"
+    data-callback="signinCallback"
+    data-clientid="56998233293.apps.googleusercontent.com"
+    data-cookiepolicy="single_host_origin"
+    data-requestvisibleactions="g-signin"
+    data-scope="profile">
+  </span>
+</span>
 
-
-
+<!-- Place this asynchronous JavaScript just before your </body> tag -->
+    <script type="text/javascript">
+      (function() {
+       var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+       po.src = 'https://apis.google.com/js/client:plusone.js';
+       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+     })();
+    </script>
 </body>
 </html>
