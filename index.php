5cba3f01c5a28ed07e0d69ce59e81396ff13fec6
Pierre <pierrerohel@gmail>
2014-01-09 14:49:42 -0500

    updates on index/insert

--- index.php
+++ index.php
@@ -4,31 +4,8 @@
 </head>
 <body>
 
-<?php
-
-include 'db.php';
-// define variables and set to empty values
-$username = $email = $ideaname = $description = NULL;
-
-if ($_SERVER["REQUEST_METHOD"] == "POST")
-{
-   $username = test_input($_POST['username']);
-   $email = test_input($_POST['email']);
-   $idea = test_input($_POST["ideaname"]);
-   $description = test_input($_POST["description"]);
-}
-
-function test_input($data)
-{
-   $data = trim($data);
-   $data = stripslashes($data);
-   $data = htmlspecialchars($data);
-   return $data;
-}
-?>
-
 <h2>PHP Form Validation Example</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<form method="post" action="insert.php">
    User Name: <input type="text" name="username">
    <br><br>
    E-mail: <input type="text" name="email">
--- insert.php
+++ insert.php
@@ -1,11 +1,35 @@
 
 <?php
+include 'db.php';
 //Insert a new idea from the form in the index page
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
+$username = $email = $ideaname = $description = NULL;
 
-mysqli_query($mysql_link,"INSERT INTO idea (user_id, name, description, Category_id)
-VALUES (0, $ideaname, $description, 0)");
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+   $username = test_input($_POST['username']);
+   $email = test_input($_POST['email']);
+   $idea = test_input($_POST["ideaname"]);
+   $description = test_input($_POST["description"]);
+}
+
+function test_input($data)
+{
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
+$sql = "INSERT INTO ideas (user_id, name, description, category_id, created_at)
+VALUES (1, $ideaname, $description, 1, NOW())"
+
+if (!mysqli_query($mysql_link,$sql))
+  {
+die('Error: ' . mysqli_error($mysql_link));
+  }
+echo "1 record added";
+
 ?>
