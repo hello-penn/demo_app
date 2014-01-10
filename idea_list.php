963bfb683405d964b8f1507a6cd3e57b62ddd16b
Pierre <pierrerohel@gmail>
2014-01-10 15:36:19 -0500

    rearranging the theme;

--- idea_list.php
+++ idea_list.php
@@ -4,7 +4,7 @@
 <head>
 	<title>Idea list</title>
 	  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/themes/my-custom-theme.css" />
+  <link rel="stylesheet" href="css/themes/hellopenn.css" />
   <link rel="stylesheet" href="css/themes/jquery.mobile.icons.min.css" />
   <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.0/jquery.mobile.structure-1.4.0.min.css" /> 
   <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script> 
@@ -19,13 +19,14 @@ if (mysqli_connect_errno())
   }
 
 $result = mysqli_query($mysql_link,"show tables;");
-
+echo "<ul>";
 while($row = mysqli_fetch_array($result))
   {
+  echo "<li>";
   echo $row[0];
-  echo "<br>";
+  echo "</li>";
   }
-
+echo "</ul>";
 ?>
 
 </body>
